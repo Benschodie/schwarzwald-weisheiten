ea4099a8a13d3188a02b24934befabc99bd29639
Benjamin Konopka <benjamin.konopka@icloud.com>
2020-07-31 09:48:11 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+let errorCount = 0;
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Get Quote from API
+async function getQuote() {
+  showLoadingSpinner();
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unbekannt";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce Font Size for long Quotes
+    if (data.quoteText.length > 100) {
+      quoteText.classList.add = "long-quote";
+    } else {
+      quoteText.classList.remove = "long-quote";
+    }
+    quoteText.innerText = data.quoteText;
+    hideLoadingSpinner();
+  } catch (error) {
+    if (errorCount <= 10) {
+      setTimeout(function () {
+        getQuote();
+      }, 500);
+      errorCount++;
+    }
+  }
+}
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuote();
